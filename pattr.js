682cc42a30e0488f2e4a4773db737777ae11f16b
jimafisk <jim.fisk@jantcu.com>
2025-12-04 20:09:46 -0500

    Focus / blur example.

--- pattr.js
+++ pattr.js
@@ -104,7 +104,7 @@ window.Pattr = {
             el.innerHTML = html;
         },
         'p-show': (el, value) => {
-            el.style.display = value ? 'block' : 'none'
+            el.style.display = value ? 'initial' : 'none'
         },
         'p-model': (el, value) => {
             el.value = value
